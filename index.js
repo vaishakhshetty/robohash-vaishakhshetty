52b498275676a1d8ee3494f5846fec92d96d0262
Vaishakh Shetty <vaishakhshetty197@gmail.com>
2021-10-03 16:16:01 +0530

    feat : promise

--- index.js
+++ index.js
@@ -1,47 +1,111 @@
-var fs = require("fs");
-const superagent = require("superagent");
+var fs = require ('fs');
+const superagent = require('superagent');
 
-const characters =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-length = 5;
 
-//function to generate a random string
-function generateString(length) {
-  let result = " ";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//------promise-----------------------
+
+/*function readFilePromise(fileLocation) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(fileLocation, (err, randomText) => {
+        if (err) {
+          reject('not able to read the file')
+        }
+        resolve(randomText)
+      })
+    })
+  }
+  function writeFilePromise(fileLocation, result) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fileLocation, result, (err) => {
+        if (err) {
+          reject('not able to write to the file')
+        }
+        resolve()
+      })
+    })
   }
 
-  return result;
-}
+  readFilePromise('./name.txt')
+   .then((randomText) => {
+     console.log(`my string is ${randomText}`);
+     return superagent.get(`https://robohash.org/${randomText}/images/random`)
+   })
+   .then((res) => {
+     console.log('String  is ', res.request.url)
+     return writeFilePromise('./roboimage.txt', res.request.url)
+   })
+   .then(() => {
+     console.log('sucessfully written the file')
+   })
+   .catch((err) => {
+     console.log(err)
+   })
 
-let roboText = generateString(length);
-console.log(roboText);
+*/
 
-//write file function -
-fs.writeFile("roboText.txt", roboText, (err) => {
-  if (err) console.log(err);
-  else {
-    console.log("File written successfully");
+//--------callback-------------
+/*fs.readFile('./name.txt','utf8', function(err, data){
+  
+    console.log(`The robo is ${data}`);
+     superagent.get(`https://robohash.org/${data}/images/random`)
+    .end((err,res)=>{
+        console.log(res.request.url)
+        fs.writeFile('./roboImage.txt', res.request.url,(err,data)=>{
+            console.log('Robo name successfully written!!');
+        })
+    })
+});*/
+
+//async-------------------------
+function readFilePromise(fileLocation) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(fileLocation, (err, randomText) => {
+        if (err) {
+          reject('not able to read the file')
+        }
+        resolve(randomText)
+      })
+    })
   }
-});
-
-//Callback -
-fs.readFile("./roboText.txt", (err, data) => {
-  if (err) {
-    console.log("error in reading the file", err.message);
-    return;
+  function writeFilePromise(fileLocation, result) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fileLocation, result, (err) => {
+        if (err) {
+          reject('not able to write to the file')
+        }
+        resolve()
+      })
+    })
   }
-  console.log(`the random text generated is ${data}`);
-  superagent.get(`https://robohash.org/${data}`).end((res) => {
-    console.log(res.body.message);
-    fs.writeFile("./roboImage.txt", res.body.message, (err) => {
-      if (err) {
-        console.log("not able to write robot image to the file", err);
-        return;
-      }
-      console.log("robot image successfully written inside the file");
-    });
-  });
-});
+  
+  
+  
+  async function getRoboPic() {
+    try {
+      const randomText = await readFilePromise('./name.txt')
+      console.log(`Robo is ${randomText}`)
+      const res = await superagent.get(
+        `https://robohash.org/${data}/images/random`
+      )
+      console.log('Robo image is ', res.request.url)
+      await writeFilePromise('./robo-image.txt', res.request.url)
+      console.log('sucessfully written the file')
+    } catch (err) {
+      throw err
+    }
+    console.log('2. complete')
+  }
+  console.log('1. start')
+  ;(async () => {
+    try {
+      await getRoboPic()
+      console.log('3. end')
+    } catch (err) {
+      console.log('3. end due to error')
+    }
+  })()
+  
+  // getRoboPic().then(() => {
+  //   console.log('3. end')
+  // })
+  
